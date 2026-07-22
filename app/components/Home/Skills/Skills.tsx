7edbb3a945f64ba62f52f01604c140202e6ee0af
Abdalluh Elsawy <elsawyabdalluh@gmail.com>
2026-07-22 18:37:38 +0300

    feat: add Skills file and implement new features

--- app/components/Home/Skills/Skills.tsx
+++ app/components/Home/Skills/Skills.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import {
+  SiJavascript,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+
+const skills = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+    percentage: 89,
+  },
+  {
+    name: "React Js",
+    icon: <SiReact />,
+    percentage: 92,
+  },
+  {
+    name: "Next Js",
+    icon: <SiNextdotjs />,
+    percentage: 92,
+  },
+  {
+    name: "Node Js",
+    icon: <SiNodedotjs />,
+    percentage: 98,
+  },
+  {
+    name: "Tailwind css",
+    icon: <SiTailwindcss />,
+    percentage: 94,
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+    percentage: 86,
+  },
+];
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
-
-export default Skills
+    <div className="pt-16 pb-16">
+      <h1 className="text-center text-3xl sm:text-4xl font-bold text-white">
+        My <span className="text-cyan-300">Skills</span>
+      </h1>
+      <div className="flex flex-wrap justify-center gap-6 mt-16">
+        {skills.map((skill) => {
+          return <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+            <div className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-1.5">
+              <div className="text-5xl mb-4 text-gray-300">{skill.icon}</div>
+              <p className="text-2xl font-semibold text-white">{skill.percentage}%</p>
+              <p className="text-purple-400 mt-1">{skill.name}</p>
+            </div>
+          </Tilt>
+        })}
+      </div>
+    </div>
+  );
+};
+export default Skills;
